fba79ecd36754ac3bf33e07d3cad0dbc25af6749
Aditi0106 <144658260+Aditi0106@users.noreply.github.com>
2024-09-29 14:35:28 +0000

    04_dateinJs.js

---
+++ 01_Basics/04_datesinJs.js
@@ -0,0 +1,34 @@
+// let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth());
+// console.log(newDate.getTime());
+
+// Q-1 Write a JavaScript code to get the current year and log it to the console.
+// let currYear = new Date()
+// console.log(currYear.getFullYear())
+
+// Q-2 Write a JavaScript code to get the current month (as a number, where 0 = January and 11 = December) and log it to the console.
+
+// console.log(currYear.getMonth());
+
+// Q-3 Write a JavaScript code to get the current day of the week (as a string, e.g., "Monday", "Tuesday", etc.) and log it to the console.
+//  let currWeek = new Date()
+//  let dayOfweek = currWeek.toLocaleString(`default`, {weekday: "long"})
+//  console.log(dayOfweek)
+
+// Q-4 Write a JavaScript function that takes a Date object as an input and returns a formatted string in the format "MM/DD/YYYY".
+// let newDate = new Date()
+// var todayDate = newDate.getDate();
+// var thisMonth = newDate.getMonth();
+// var thisYear = newDate.getFullYear();
+// var finalDate = todayDate +'/'  + thisMonth + '/' + thisYear;
+// console.log(finalDate)
+
+// Q-5 Write a JavaScript function that takes two Date objects as inputs and returns the difference between them in days.
+
+
+    
+
+  
+
